e16085c9889538d7b1e3cdc0f01d429528e2c046
thaopv <phamthao03596@gmail.com>
2019-03-21 21:06:47 +0700

    Update gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Seosight - Blog</title>
 
-    <link rel='stylesheet' href='{{ mix('css/mix.css') }}' type='text/css'>
+    <link rel='stylesheet' href='{{ mix('css/app.css') }}' type='text/css'>
 
     <!--Styles for RTL-->
 
